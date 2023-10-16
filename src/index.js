95de40447e9ef84a717e62ddd339dcd91d476bed
Kara Brightwell <kara@153.io>
2023-10-16 22:41:33 +0100

    use a single onscreen canvas

--- src/index.js
+++ src/index.js
@@ -24,15 +24,12 @@ input.addEventListener('change', () => {
 		const width = landscape ? 800 : (800 * aspect)
 		const height = landscape ? (800 / aspect) : 800
 
-		const offscreen = new OffscreenCanvas(width, height)
-		const octx = offscreen.getContext('2d')
-
-		octx.drawImage(image, 0, 0, width, height)
-
-		const imageData = octx.getImageData(0, 0, width, height)
-
 		canvas.width = width
 		canvas.height = height
+
+		ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height)
+
+		const imageData = ctx.getImageData(0, 0, width, height)
 
 		worker.postMessage({ action: 'setImageData', data: imageData }, [imageData.data.buffer])
 
@@ -44,7 +41,7 @@ input.addEventListener('change', () => {
 			})
 		}, 100)
 
-		worker.addEventListener('message', event => ctx.putImageData(event.data, 0, 0))
+		worker.addEventListener('message', event => ctx.putImageData(event.data, 0, 0), {once: true})
 
 	}, {once: true})
 
